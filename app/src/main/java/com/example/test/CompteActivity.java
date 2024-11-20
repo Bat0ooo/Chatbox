9774efc37dd5327ed0de0d19ead5f89d88385af3
Bat0ooo <baptiste.ferrand31@gmail.com>
2024-11-20 09:49:13 +0100

    Champs creation de comte ok

--- app/src/main/java/com/example/test/CompteActivity.java
+++ app/src/main/java/com/example/test/CompteActivity.java
@@ -1,6 +1,13 @@
 package com.example.test;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class CompteActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private EditText login, password, password2;
+    private Button create;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +33,28 @@ public class CompteActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        login = findViewById(R.id.Create_login);
+        password = findViewById(R.id.Create_Password);
+        password2 = findViewById(R.id.Create_Password2);
+        create = findViewById(R.id.Create_button);
+
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Vérifier si les champs sont remplis
+                if (login.getText().toString().isEmpty() || !(password.getText().toString().equals(password2.getText().toString())) || password.getText().toString().isEmpty() || password2.getText().toString().isEmpty()) {
+                    TextView errorText = findViewById(R.id.errorText);
+                    errorText.setText("Champs incorrect !");
+                    errorText.setVisibility(View.VISIBLE);
+                } else {
+                    Intent intent = new Intent(CompteActivity.this, AuthentificationActivity.class);
+                    startActivity(intent);
+                }
+            }
         });
     }
 }
